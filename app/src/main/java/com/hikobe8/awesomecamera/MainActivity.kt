51137a85fa232be4229cd122a1b81796c670207e
yurui <yurui@palmax.cn>
2019-04-24 15:08:56 +0800

    add Firebase AI support

--- app/src/main/java/com/hikobe8/awesomecamera/MainActivity.kt
+++ app/src/main/java/com/hikobe8/awesomecamera/MainActivity.kt
@@ -1,12 +1,109 @@
 package com.hikobe8.awesomecamera
 
-import android.support.v7.app.AppCompatActivity
+import android.Manifest
+import android.graphics.*
 import android.os.Bundle
+import android.support.annotation.NonNull
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.face.FirebaseVisionFace
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceContour
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceLandmark
+import com.tbruyelle.rxpermissions2.RxPermissions
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val subscribe = RxPermissions(this)
+            .request(Manifest.permission.CAMERA)
+            .subscribe { granted ->
+                if (granted) {
+                    initViews()
+                } else {
+                    // Oups permission denied
+                    finish()
+                }
+            }
+
     }
+
+    private fun initViews() {
+        setContentView(R.layout.activity_main)
+        val bitmapOptions = BitmapFactory.Options().apply {
+            inSampleSize = 2
+        }
+        val processBitmap = BitmapFactory.decodeResource(resources, R.drawable.man, bitmapOptions)
+        // High-accuracy landmark detection and face classification
+        val options = FirebaseVisionFaceDetectorOptions.Builder()
+            .setPerformanceMode(FirebaseVisionFaceDetectorOptions.ACCURATE)
+            .setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
+            .setContourMode(FirebaseVisionFaceDetectorOptions.ALL_CONTOURS)
+            .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
+            .build()
+
+        val image = FirebaseVisionImage.fromBitmap(processBitmap)
+        val detector = FirebaseVision.getInstance()
+            .getVisionFaceDetector(options)
+        val result = detector.detectInImage(image)
+            .addOnSuccessListener {
+                Log.e("fuck", it.toString())
+                for (face in it) {
+                    val bounds = face.getBoundingBox()
+                    val copy = processBitmap.copy(Bitmap.Config.ARGB_8888, true)
+                    val c = Canvas(copy)
+                    c.drawRect(bounds, Paint().apply {
+                        color = Color.RED
+                        style = Paint.Style.STROKE
+                        strokeWidth = 4f
+                    })
+                    iv.setImageBitmap(copy)
+                    val rotY = face.getHeadEulerAngleY()  // Head is rotated to the right rotY degrees
+                    val rotZ = face.getHeadEulerAngleZ()  // Head is tilted sideways rotZ degrees
+
+                    // If landmark detection was enabled (mouth, ears, eyes, cheeks, and
+                    // nose available):
+                    val leftEar = face.getLandmark(FirebaseVisionFaceLandmark.LEFT_EAR)
+                    if (leftEar != null) {
+                        val leftEarPos = leftEar!!.getPosition()
+                    }
+
+                    // If contour detection was enabled:
+                    val leftEyeContour = face.getContour(FirebaseVisionFaceContour.LEFT_EYE).getPoints()
+                    val upperLipBottomContour = face.getContour(FirebaseVisionFaceContour.UPPER_LIP_BOTTOM).getPoints()
+
+                    // If classification was enabled:
+                    if (face.getSmilingProbability() != FirebaseVisionFace.UNCOMPUTED_PROBABILITY) {
+                        val smileProb = face.getSmilingProbability()
+                    }
+                    if (face.getRightEyeOpenProbability() != FirebaseVisionFace.UNCOMPUTED_PROBABILITY) {
+                        val rightEyeOpenProb = face.getRightEyeOpenProbability()
+                    }
+
+                    // If face tracking was enabled:
+                    if (face.getTrackingId() != FirebaseVisionFace.INVALID_ID) {
+                        val id = face.getTrackingId()
+                    }
+                }
+            }
+            .addOnFailureListener(
+                object : OnFailureListener {
+                    override fun onFailure(@NonNull e: Exception) {
+                        // Task failed with an exception
+                        // ...
+                        Log.e("fuck", e.toString())
+                    }
+                })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
 }
---
+++ app/src/main/java/com/hikobe8/awesomecamera/camera/RayCameraView.kt
@@ -0,0 +1,40 @@
+package com.hikobe8.awesomecamera.camera
+
+import android.content.Context
+import android.hardware.Camera
+import android.util.AttributeSet
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+
+/***
+ *  Author : ryu18356@gmail.com
+ *  Create at 2019-04-23 15:55
+ *  description : SurfaceView with Camera
+ */
+class RayCameraView(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) :
+    SurfaceView(context, attrs, defStyleAttr), SurfaceHolder.Callback {
+
+    constructor(context: Context?):this(context, null, 0)
+
+    constructor(context: Context?, attrs: AttributeSet?):this(context, attrs, 0)
+
+    init {
+        holder.addCallback(this)
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
+
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder?) {
+
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder?) {
+        val camera = Camera.open()
+        camera.setPreviewDisplay(holder)
+        camera.startPreview()
+    }
+
+
+}
